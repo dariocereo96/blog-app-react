49665b63f4f691202d40824b3203ac25e5175a58
dariocerezo96 <pablodariocerezo@gmail.com>
2021-12-25 14:46:21 -0500

    Modificacion de archivos

--- src/App.js
+++ src/App.js
@@ -2,37 +2,19 @@
 import React, { useState } from 'react';
 import Login from './components/Login';
 import axios from 'axios';
+import NavBar from './components/NavBar';
 
 function App() {
 
   const getData=()=>{
      axios.get('http://127.0.0.1:8000/api/articles', {
      responseType: 'json'
-   }).then((data)=>console.log(data));
+   }).then((data)=>console.log(data.data));
   } 
 
   return (
     <div className="App container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Blogger</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-              <a className="nav-link" href="#">Articles</a>
-              <a className="nav-link" href="#">Etiquetas</a>
-              <a className="nav-link disabled">Mas recientes</a>
-            </div>
-          </div>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" onClick={getData()}>Search</button>
-            </form>
-        </div>
-      </nav>
+      {/* <NavBar/> */}
       <Login/>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,25 @@
 
 import React, { useState } from 'react';
-
+import axios from 'axios';
 function Login() {
-  const [count, setCount] = useState(0);
+  const [datos, setDatos] = useState([]);
+  
 
+  const login=async()=>{
+    await axios.get('http://127.0.0.1:8000/api/articles', {
+     responseType: 'json'
+    }).then((response)=>{
+      setDatos(response.data);
+    }).catch((error)=>{
+      console.log(error);
+    });
+    
+  }
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
+    <>
+     <button onClick={login}>Login</button>
+     <p>{datos}</p>
+    </>
   );
 }
 
